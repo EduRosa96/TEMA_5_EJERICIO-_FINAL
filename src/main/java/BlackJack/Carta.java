2581e06e3b71727619b7b8c3ba757ed9f76e4cb8
EduRosa96 <eduardororo96@gmail.com>
2024-02-26 19:27:34 +0100

    Clase Carta

--- src/main/java/BlackJack/Carta.java
+++ src/main/java/BlackJack/Carta.java
@@ -1,7 +1,7 @@
 package BlackJack;
 
 public class Carta {
-
+    //PARÁMETROS
     private Palo palo;
 
     enum Palo {
@@ -11,11 +11,14 @@ public class Carta {
     ;
     private int numCarta;
 
+    //CONSTRUCTOR
     public Carta(Palo palo, int numCarta) {
+
         this.palo = palo;
         this.numCarta = numCarta;
     }
 
+    //GETTERS
     public Palo getPalo() {
         return palo;
     }
@@ -25,6 +28,36 @@ public class Carta {
             return numCarta;
         } else {
             System.out.println("Introduce un número válido");
+            return -1;
         }
+    }
+
+    public int getValor() {
+        if (numCarta == 1) {
+            return numCarta = 11;
+        } else if (numCarta == 11 || numCarta == 12 || numCarta == 13) {
+            return numCarta = 10;
+        } else {
+            return numCarta;
+        }
+    }
+
+    public String mostrarNumero() {
+        if (numCarta == 1) {
+            return "AS";
+        } else if (numCarta == 11) {
+            return "J";
+        } else if (numCarta == 12) {
+            return "Q";
+        } else if (numCarta == 13) {
+            return "K";
+        } else {
+            return Integer.toString(numCarta);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + numCarta + " - " + palo + " ]";
     }
 }
